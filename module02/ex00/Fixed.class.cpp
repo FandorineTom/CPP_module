1faf32fccf241fc94959661432d911ae6ea8131e
glaisan <ellenhanx@gmail.com>
2021-04-03 13:42:33 +0300

    ex00 done

---
+++ module02/ex00/Fixed.class.cpp
@@ -0,0 +1,30 @@
+#include "Fixed.class.hpp"
+
+Fixed::Fixed() : _fixed_point_value(0) {
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &copy_fixed) {
+	std::cout << "Copy constructor called\n";
+	this->_fixed_point_value = copy_fixed.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &to_assign) {
+	std::cout << "Assignation operator called\n";
+	_fixed_point_value = to_assign.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called\n";
+	return (_fixed_point_value);
+}
+
+void Fixed::setRawBits(int const value) {
+	std::cout << "getRawBits member function called\n";
+	_fixed_point_value = value;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called\n";
+}
--- module02/ex00/Fixed.class.hpp
+++ module02/ex00/Fixed.class.hpp
@@ -1,10 +1,23 @@
 #ifndef FIXED_H
 #define FIXED_H
 
+#include <iostream>
+
 class Fixed {
 	private:
 		int					_fixed_point_value;
-		static const int	_num_frac_bits;
-}
+		static const int	_num_frac_bits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed &copy_fixed);
+		~Fixed();
+
+		Fixed&	operator=(const Fixed &to_assign);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const value);
+	
+};
 
 #endif
