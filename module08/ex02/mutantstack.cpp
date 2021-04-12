5f04325523d09261edf6b81cc4ff972e77fbdb02
glaisan <ellenhanx@gmail.com>
2021-04-13 01:42:42 +0300

    problems with main, dont know what to do, found a mistake in ex01, doesnt really find shortest span

---
+++ module08/ex02/mutantstack.cpp
@@ -0,0 +1,70 @@
+#include "mutantstack.hpp"
+
+template<typename T>
+MutantStack<T>::MutantStack() : std::stack<T>() {}
+
+template<typename T>
+MutantStack<T>::MutantStack(const MutantStack &copy) : std::stack<T>(copy.c) {}
+
+template<typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &assign) {
+	this->c = assign.c;
+	return *this;
+}
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin() {
+	return typename MutantStack<T>::iterator(&this->top() - (this->size() - 1));
+}
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end() {
+	return typename MutantStack<T>::iterator(&this->top() + 1);
+}
+
+template<typename T>
+MutantStack<T>::~MutantStack() {}
+
+template<typename T>
+MutantStack<T>::iterator::iterator(T *ptr) : _ptr(ptr) {}
+
+template<typename T>
+typename MutantStack<T>::iterator &MutantStack<T>::iterator::operator=(const iterator &assign) {
+	if (this == &assign)
+		return *this;
+	*_ptr = assign.getT();
+	return *this;
+}
+
+template<typename T>
+MutantStack<T>::iterator::~iterator() {}
+
+template<typename T>
+T		*MutantStack<T>::iterator::getT() const {return _ptr;}
+
+template<typename T>
+bool	MutantStack<T>::iterator::operator==(iterator const &it) const {return _ptr == it._ptr;}
+template<typename T>
+bool	MutantStack<T>::iterator::operator!=(iterator const &it) const {return _ptr != it._ptr;}
+template<typename T>
+bool	MutantStack<T>::iterator::operator>=(iterator const &it) const {return _ptr >= it._ptr;}
+template<typename T>
+bool	MutantStack<T>::iterator::operator<=(iterator const &it) const {return _ptr <= it._ptr;}
+template<typename T>
+bool	MutantStack<T>::iterator::operator<(iterator const &it) const {return _ptr < it._ptr;}
+template<typename T>
+bool	MutantStack<T>::iterator::operator>(iterator const &it) const {return _ptr > it._ptr;}
+template<typename T>
+T		&MutantStack<T>::iterator::operator*() const {return *_ptr;}
+
+template<typename T>
+typename MutantStack<T>::iterator	&MutantStack<T>::iterator::operator++() {
+	++_ptr;
+	return *this;
+}
+
+template<typename T>
+typename MutantStack<T>::iterator	&MutantStack<T>::iterator::operator--() {
+	--_ptr;
+	return *this;
+}
---
+++ module08/ex02/mutantstack.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+#include <stack>
+#include <iterator>
+#include <string>
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+	public:
+		MutantStack();
+		MutantStack(const MutantStack &copy);
+		MutantStack &operator=(const MutantStack &assign);
+		virtual ~MutantStack();
+
+		class iterator : public std::iterator<std::input_iterator_tag, T> {
+			private:
+				T	*_ptr;
+			
+			public:
+				iterator();
+				iterator(T *ptr);
+				iterator(const iterator &copy);
+				iterator &operator=(const iterator &assign);
+				~iterator();
+
+				T	*getT() const;
+				bool	operator==(const iterator &iterator) const;
+				bool	operator!=(const iterator &iterator) const;
+				bool	operator>=(const iterator &iterator) const;
+				bool	operator<=(const iterator &iterator) const;
+				bool	operator<(const iterator &iterator) const;
+				bool	operator>(const iterator &iterator) const;
+
+				iterator	&operator++();
+				iterator	&operator--();
+				T			&operator*() const;
+		};
+
+		iterator	begin();
+		iterator	end();
+};
